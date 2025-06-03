6fe9880d584b8ab940c23bdf5932c80ddb7bc3c8
JafetVLM <jafetvalentino10@gmail.com>
2025-06-02 22:27:41 -0600

    Inicio le faltan funciones

--- app/src/main/java/com/example/medipet/MainActivity.java
+++ app/src/main/java/com/example/medipet/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.medipet;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +14,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-
+ImageView img_citas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        img_citas.findViewById(R.id.img_citas);
+        img_citas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NuevaActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
