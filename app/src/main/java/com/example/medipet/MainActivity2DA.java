c45d1b76c6a5249a906f09ce6500038af652bd81
Dani <allandanielgarciacastro@gmail.com>
2025-06-08 15:57:23 -0600

    OK

--- app/src/main/java/com/example/medipet/MainActivity2DA.java
+++ app/src/main/java/com/example/medipet/MainActivity2DA.java
@@ -10,105 +10,20 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-// Descomenta si vas a usar Volley
-/*
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
-import java.util.HashMap;
-import java.util.Map;
-*/
-
-public class MainActivity2DA extends AppCompatActivity /*implements View.OnClickListener*/ {
+public class MainActivity2DA extends AppCompatActivity  {
 
     EditText txtNom, txtApe, txtMail, txtTel, txtPass;
     Button btnCreate;
 
-    // Descomenta si usas Volley
-    // RequestQueue requestQueue;
-    // private static final String URL1 = "http://192.168.0.6/android/saves.php";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-
-        // Asegúrate de que este layout exista y esté bien hecho
         setContentView(R.layout.activity_main_activity2_da);
 
-        // Descomenta si usas Volley
-        // requestQueue = Volley.newRequestQueue(this);
-
-        initUI();
-
-        // Descomenta si implementas View.OnClickListener
-        // btnCreate.setOnClickListener(this);
     }
 
-    private void initUI() {
-        txtNom = findViewById(R.id.txtnombre);
-        txtApe = findViewById(R.id.txtapellidos);
-        txtTel = findViewById(R.id.txtnum);
-        txtMail = findViewById(R.id.txtmail);
-        txtPass = findViewById(R.id.txtpass);
-        btnCreate = findViewById(R.id.btnCreate);
-    }
-
-    // Si usas View.OnClickListener, descomenta este bloque
-    /*
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-
-        if (id == R.id.btnCreate) {
-            String nombre = txtNom.getText().toString().trim();
-            String apellido = txtApe.getText().toString().trim();
-            String telefono = txtTel.getText().toString().trim();
-            String email = txtMail.getText().toString().trim();
-            String pass = txtPass.getText().toString().trim();
-
-            createUser(nombre, apellido, telefono, email, pass);
-        }
-    }
-
-    private void createUser(final String nombre, final String apellido, final String telefono, final String email, final String pass) {
-        StringRequest stringRequest = new StringRequest(
-                Request.Method.POST,
-                URL1,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Toast.makeText(MainActivity2DA.this, "Usuario creado correctamente", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(MainActivity2DA.this, MainActivityDA.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity2DA.this, "Error al crear usuario", Toast.LENGTH_SHORT).show();
-                    }
-                }
-        ) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-                params.put("nombre", nombre);
-                params.put("apellido", apellido);
-                params.put("telefono", telefono);
-                params.put("email", email);
-                params.put("contraseña", pass);
-                return params;
-            }
-        };
-
-        requestQueue.add(stringRequest);
-    }
-    */
 }
--- app/src/main/java/com/example/medipet/MainActivityDA.java
+++ app/src/main/java/com/example/medipet/MainActivityDA.java
@@ -6,30 +6,20 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.android.material.button.MaterialButton;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 public class MainActivityDA extends AppCompatActivity {
 
-    MaterialButton btnCreate, btnRegistrarse;
-    EditText editEmail, editPassword;
-
-    ImageView img_citas,img_agregar,img_home,img_per;
+    Button btnCreate, btnRegistrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,106 +28,7 @@ public class MainActivityDA extends AppCompatActivity {
 
         btnCreate = findViewById(R.id.btnCreate);
         btnRegistrarse = findViewById(R.id.btnregistro);
-        editEmail = findViewById(R.id.txtmail);
-        editPassword = findViewById(R.id.txtpass);
-
-        final Animation rotateAnim = AnimationUtils.loadAnimation(this, R.anim.btn_scale);
-
-        btnCreate.setOnClickListener(v -> {
-            v.startAnimation(rotateAnim);
-            loginUser();
-        });
-
-        btnRegistrarse.setOnClickListener(v -> {
-            v.startAnimation(rotateAnim);
-            v.postDelayed(() -> {
-                startActivity(new Intent(MainActivityDA.this, MainActivity2DA.class));
-                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_in_right);
-            }, rotateAnim.getDuration());
-        });
 
 
-        img_home = findViewById(R.id.img_home);
-        img_home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(MainActivityDA.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-        img_citas = findViewById(R.id.img_citas);
-        img_citas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(MainActivityDA.this, activity_sucursales.class);
-                startActivity(intent);
-            }
-        });
-        img_agregar = findViewById(R.id.img_agregar);
-        img_agregar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(MainActivityDA.this, activity_dialog_cita.class);
-                startActivity(intent);
-            }
-        });
-
-    }
-
-    private void loginUser() {
-        String email = editEmail.getText().toString().trim();
-        String password = editPassword.getText().toString().trim();
-
-        Log.d("LOGIN", "Email: " + email + " | Password: " + password);
-
-        if (email.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        String url = "http://192.168.0.7/android/login.php";
-
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                response -> {
-                    try {
-                        JSONObject jsonResponse = new JSONObject(response.trim());
-                        String status = jsonResponse.getString("status");
-
-                        if (status.equals("success")) {
-                            String nombreUsuario = jsonResponse.getString("nombre");
-                            Toast.makeText(MainActivityDA.this, "Login exitoso", Toast.LENGTH_SHORT).show();
-
-                            Intent intent = new Intent(MainActivityDA.this, Usuario.class);
-                            intent.putExtra("email", email);
-                            intent.putExtra("nombre", nombreUsuario);  // Pasamos el nombre
-                            startActivity(intent);
-                            finish();
-                        } else {
-                            String message = jsonResponse.getString("message");
-                            Toast.makeText(MainActivityDA.this, "Error: " + message, Toast.LENGTH_SHORT).show();
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                        Toast.makeText(MainActivityDA.this, "Error parseando respuesta", Toast.LENGTH_SHORT).show();
-                    }
-                },
-                error -> {
-                    Toast.makeText(MainActivityDA.this, "Error de red: " + error.getMessage(), Toast.LENGTH_SHORT).show();
-                }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("email", email);
-                params.put("password", password);
-                return params;
-            }
-        };
-
-
-        RequestQueue queue = Volley.newRequestQueue(this);
-        queue.add(stringRequest);
     }
 }
